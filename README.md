b97bc15e22fe64e9a044f0a456f26bb00d5afb39
Dently <104347609+yltcode@users.noreply.github.com>
2022-04-30 13:33:27 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Автоматическое комментирование постов в Telegram
+
+[![license](https://shields.io/github/license/yltcode/telegram-auto-comment?style=flat-square&color=blue)](https://www.gnu.org/licenses/quick-guide-gplv3.ru.html)
+
+[![code-size](https://shields.io/github/languages/code-size/yltcode/telegram-auto-comment?style=flat-square&color=orange)](https://github.com/yltcode/telegram-auto-comment)
+
+## Установка
+- Клонирование репозитория
+
+```sh
+git clone https://github.com/yltcode/telegram-auto-comment
+```
+
+- Установка зависимостей
+
+```sh
+pip3 install -r requirements.txt
+```
+
+- Редактирование конфига
+
+В инструкции будет использоваться `nano`, а Вы можете использовать любой другой текстовый редактор
+
+```sh
+nano config.yaml
+```
+
+И изменяем сообщения из `messages` на свои
+
+- Запускаем
+
+```sh
+python3 main.py
+```
+
+Вводим номер телефона и код, который придет в Telegram
+
+Если написано `Started for ...`, значит все верно
+
+## Возможные ошибки
+
+- #### The SSL module is not available
+
+Отсутствует или не обновлен пакет openssl
+
+```sh
+apt update
+apt install --only-upgrade openssl
+```
---
+++ main.py
@@ -0,0 +1,64 @@
+#    https://github.com/yltcode/telegram-auto-comment
+#    Copyright (C) 2022-present  dently
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+import yaml
+import random
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.errors import MsgIdInvalid
+
+directory = os.path.dirname(__file__)
+
+if directory != os.getcwd():
+    os.chdir(directory)
+
+if os.path.exists("session.session-journal"):
+    os.remove("session.session-journal")
+
+with open("config.yaml", "r", encoding="utf-8") as file:
+    config = yaml.safe_load(file)
+
+api_id = config["api_id"]
+api_hash = config["api_hash"]
+messages = config["messages"]
+
+app = Client(
+    "session",
+    api_id,
+    api_hash
+)
+
+with app:
+    me = app.get_me()
+    print("Started for", me.first_name)
+
+
+@app.on_message(filters.channel & ~filters.me)
+async def handler(app: Client, message: Message):
+    try:
+        text = random.choice(messages)
+        msg = await app.get_discussion_message(
+            message.chat.id, message.id
+        )
+        await msg.reply(text)
+    except MsgIdInvalid:
+        ...
+
+
+if __name__ == "__main__":
+    app.run()
